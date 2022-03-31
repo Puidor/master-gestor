7304b36b4ab39506fcf16cbadb89fa0a2bc7073e
Puidor <c.adriano.silva@hotmail.com>
2022-03-31 11:06:59 -0300

    Some Refactoring

--- app/Http/Controllers/ContatosController.php
+++ app/Http/Controllers/ContatosController.php
@@ -4,15 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\SiteContato;
+use App\Models\MotivoContato;
 
 class ContatosController extends Controller
 {
     public function contatos(Request $request){
-        $motivo_contatos = [
-            '1' => 'Dúvida',
-            '2' => 'Elogio',
-            '3' => 'Reclamação',
-        ];
+        //Recebe todos os registros da tabela motivo_contatos
+        $motivo_contatos = MotivoContato::all();
         return view('site.contato', ['motivo_contatos' => $motivo_contatos]);
     }
 
