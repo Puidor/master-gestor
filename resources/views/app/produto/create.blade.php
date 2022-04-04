e60e50739577d7d38b48fbe9d9b3fc40d2ce183a
Puidor <c.adriano.silva@hotmail.com>
2022-04-04 10:49:35 -0300

    Add produto.store

--- resources/views/app/produto/create.blade.php
+++ resources/views/app/produto/create.blade.php
@@ -15,7 +15,7 @@
         </div>
         <div class="informacao-pagina">
             <div style="width: 30%; margin-left: auto; margin-right: auto;">
-                <form action="" method="post"> @csrf
+                <form action="{{ route('produto.store') }}" method="post"> @csrf
                     {{-- Input Nome --}}
                     <input type="text" name="nome" value="" placeholder="Nome" class="borda-preta">
 
