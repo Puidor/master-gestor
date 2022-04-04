3134f27efc790bc82dcd1c57e9251e8686743ff2
Puidor <c.adriano.silva@hotmail.com>
2022-04-04 15:29:21 -0300

    componente form edit e create produto

--- resources/views/app/produto/edit.blade.php
+++ resources/views/app/produto/edit.blade.php
@@ -19,30 +19,11 @@
 
         <div class="informacao-pagina">
             <div style="width: 30%; margin-left: auto; margin-right: auto;">
-                <form method="post" action="{{ route('produto.update', ['produto' => $produto->id]) }}">
-                    @csrf    
-                    @method('PUT') {{-- Método como será o update! - Pode ser PUT ou PATCH --}}
-
-                    <input type="text" name="nome" value="{{ $produto->nome ?? old('nome') }}" placeholder="Nome" class="borda-preta">
-                    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
-
-                    <input type="text" name="descricao" value="{{ $produto->descricao ?? old('descricao') }}" placeholder="Descrição" class="borda-preta">
-                    {{ $errors->has('descricao') ? $errors->first('descricao') : '' }}
-
-                    <input type="text" name="peso" value="{{ $produto->peso ?? old('peso') }}" placeholder="peso" class="borda-preta">
-                    {{ $errors->has('peso') ? $errors->first('peso') : '' }}
-
-                    <select name="unidade_id">
-                        <option>-- Selecione a Unidade de Medida --</option>
-
-                        @foreach ($unidades as $unidade)
-                            <option value="{{ $unidade->id }}" {{ ( $produto->unidade_id ?? old('unidade_id') ) == $unidade->id ? 'selected' : '' }} >{{ $unidade->descricao }}</option>
-                        @endforeach
-                        {{ $errors->has('unidade_id') ? $errors->first('unidade_id') : '' }}
-                    </select>
-                    
-                    <button type="submit" class="borda-preta">Cadastrar</button>
-                </form>
+                @component('app.produto._components.form_create_edit', [
+                    'produto' => $produto ?? null,
+                    'unidades' => $unidades ?? null,
+                ])
+                @endcomponent
             </div>
         </div>
 
